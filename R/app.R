46806545de47ba7530760c038f1b94952be7752e
mandyhee <homengni0316@gmail.com>
2019-12-17 14:48:55 -0500

    update shiny

--- R/app.R
+++ R/app.R
@@ -9,14 +9,21 @@
 
 library(shiny)
 library(shinydashboard)
+library(tidyverse)
+library(tidytext)
+library(tm)
+library(glmnet)
 
 # load data, function
-source("~/Desktop/untitled folder/svm_testing.R")
+LR_cov_model = readRDS("./rds/logistic_model_cov.rds")
+col_names_cov = readRDS("./rds/col_names_cov.rds")
+col_names_cov[which(col_names_cov == "break")] = "brk"
+
 
 # airline list
-airline = c("Delta Airline", "American Airline", "United Airline", "Jetblue Airline")
-cabin = c("Econony", "Premium Economy", "Business", "First Class")
-traveller = c("Business", "Couple", "Family", "Solo")
+# airline = c("Delta Airline", "American Airline", "United Airline", "Jetblue Airline")
+US = c("US", "Not US")
+Rich = c("Rich", "Poor")
 
 # Define UI for application that draws a histogram
 header <- dashboardHeader(title = "Customer Review Prediction")
@@ -37,9 +44,9 @@ body <- dashboardBody(
             status = "primary"),
         
         box(title = "Dropdown selection",
-            selectInput("selection_airline", label = "Select type of Airline", choices = airline),
-            selectInput("selection_cabin", label = "Select type of Cabin", choices = cabin),
-            selectInput("selection_traveller", label = "Select type of Traveller", choices = traveller),
+            #selectInput("selection_airline", label = "Select type of Airline", choices = airline),
+            selectInput("selection_country", label = "Select type of Cabin", choices = US),
+            selectInput("selection_income", label = "Select type of Traveller", choices = Rich),
             status = "primary")
     ),
     
@@ -62,26 +69,62 @@ body <- dashboardBody(
 # Define server logic required to draw a histogram
 server <- function(input, output){
     output$result = renderText({
-        test = c()
-        traveller = input$selection_traveller
-        overall_r = input$overall_rating
-        cabin_r = input$cabin_rating
-        wifi_r = input$wifi_connectivity
-        if (traveller == "Business"){
-            pat = c(1,0,0,0)
-            test = c(test, pat)
-        } else if (traveller == "Couple"){
-            pat = c(0,1,0,0)
-            test = c(test, pat)
-        } else if (traveller == "Family"){
-            pat = c(0,0,1,0)
-            test = c(test, pat)
-        } else{
-            pat = c(0,0,0,1)
-            test = c(test, pat)
+        ##### dropdown ########
+        country = input$selection_country
+        income = input$selection_income
+        if (country == "US"){ us_cat = 1
+        } else {  us_cat = 0} 
+        
+        if (income == "Rich"){ rich_cat = 1
+        } else { rich_cat = 0 } 
+        
+        ###### comment ########
+        comment = input$text_comment
+        test = data.frame(ID = 1, content = comment) 
+        
+        test = test %>% 
+            unnest_tokens(word, content)
+        bing = get_sentiments(lexicon = "bing")
+        
+        
+        test_bing = test %>%
+            select(ID, word) %>%
+            unique() %>%
+            left_join(bing, by = "word") %>%
+            na.omit()
+        
+        # add UNK if there are no words
+        if (dim(test_bing)[1] == 0){
+            UNK = data.frame(ID = 1, word = "UNK", sentiment = NA)
+            test_bing = rbind(test_bing, UNK)
+        } else {test_bing = test_bing}
+        
+        test_dtm = test_bing %>%
+            #get word count per document to pass to cast_dtm
+            count(ID, word, sort = TRUE) %>%
+            ungroup() %>%
+            #create a DTM with docs as rows and words as columns
+            cast_dtm(document = ID, term = word, value = n)
+        
+        # create the big matrix contain all the words (like in training set)
+        test_input = colnames(as.matrix(test_dtm))
+        test_matrix = matrix(0, nrow = 1, ncol = length(col_names_cov))
+        colnames(test_matrix) = col_names_cov
+        for(i in test_input){
+            if (i == "break"){
+                i == "brk"
+            }
+            pos = as.vector(grep(i, colnames(test_matrix)))[1]
+            test_matrix[1,pos] = 1
         }
-        test = c(test, overall_r,cabin_r,wifi_r)
-        result = predict(classifier, newdata = t(as.matrix(test)))
+        
+        test_matrix[1,3513] = us_cat
+        test_matrix[1,3514] = rich_cat
+        
+        
+        result = ifelse(predict.glmnet(LR_cov_model, test_matrix, type="response") > 0.5,1,0)
+        
+        # result = predict(classifier, newdata = t(as.matrix(test)))
         if (as.vector(result) == 0){
             paste("Not recommend")
         } else {paste("Recommend!!")}
